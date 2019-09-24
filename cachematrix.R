a8a3a3eb02b684f74cba5086daa1bdaf09312b67
Otto Taute <otto.taute@outlook.com>
2019-09-24 14:59:21 +0200

    Add comments describing the purpose of the module and functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,23 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions try to improve performance when calculating the
+## inverse of a matrix. It does so by first constructing a cached
+## version of a matrix, which stores its inverse in an internal
+## cache.
+## The inverse is only calculated the first time and any subsequent
+## calls will simply return the cached inverse.
 
-## Write a short comment describing this function
+## Takes a matrix and returns a list of operations that can be
+## performed on the matrix (get, set, getinverse, setinverse)
 
 makeCacheMatrix <- function(x = matrix()) {
 
 }
 
 
-## Write a short comment describing this function
+## Takes a special matrix as provided by makeCacheMatrix and
+## returns the inverse of that matrix. If the inverse has not
+## been calculated before, it is calculated and stored in the
+## matrix internal cache, otherwise the inverse is returned
+## directly.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
